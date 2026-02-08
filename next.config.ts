cd081d5d7cbf4fec59cb6f32c11f2d4de82f5ce1
sarvesh <srbhanarkar05@gmail.com>
2026-02-08 13:00:50 +0530

    fixed compilation issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "wagmi",
+      "viem",
+      "@tanstack/react-query",
+    ],
+  },
 };
 
 export default nextConfig;
